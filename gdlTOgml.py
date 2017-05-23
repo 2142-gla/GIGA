c14b8645219cceba97ca86ee73030a4ab2004ba2
2142-gla <2142695s@student.gla.ac.uk>
2017-05-23 10:36:11 +0100

    Add files via upload

---
+++ gdlTOgml.py
@@ -0,0 +1,36 @@
+"""
+Convertion program to create a gml (graph modelling language) from a Group based Iterative Group Analysis (GiGA) in 
+ .gdl format
+ Robin Shaw May/August 2017
+ Version 1 - started 23/5/17
+"""
+
+# Functions
+
+
+# Variables
+
+
+# 'main' code block
+
+# Load the gdl file and create the gml file
+
+# Iterate through the gdl file
+
+# Build up the gene list. This requires giving each gene an unique id
+# and record its name and GO (Gene Ontology) feature.
+
+# Identify each new Functional Class (FC) as this will be a new sub-graph in the network
+# Load each new FC in to a dictionary
+
+# For each individual FC gather evidence (edges) between each node (gene)
+
+# Identify the end of a FC and end of the file.
+
+
+# Create a header and footer for the gml file
+# Create a sub graph for each FC
+# Create the nodes for the graph
+# Create the edges
+
+# Close the files
